83f381c8de966c38a084098edf02eeb2bd234ab4
Ben <b.cylin@gmail.com>
2017-05-08 18:35:22 +0800

    Set up an example project

--- Example/Pickle/AppDelegate.swift
+++ Example/Pickle/AppDelegate.swift
@@ -16,7 +16,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.backgroundColor = UIColor.white
+        window?.rootViewController = UINavigationController(rootViewController: ViewController())
+        window?.makeKeyAndVisible()
         return true
     }
 
--- Example/Pickle/ViewController.swift
+++ Example/Pickle/ViewController.swift
@@ -9,7 +9,97 @@
 //
 
 import UIKit
+import Pickle
+import Photos
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController,
+    UITableViewDataSource,
+    UITableViewDelegate,
+    ImagePickerControllerDelegate {
+
+    private enum Section: Int {
+        case `default`
+
+        static var count: Int {
+            var count = 0
+            while let _ = Section(rawValue: count) { count += 1 }
+            return count
+        }
+
+        var title: String {
+            switch self {
+            case .default: return "Default appearance"
+            }
+        }
+    }
+
+    private lazy var tableView: UITableView = {
+        let tableView = UITableView(frame: .zero, style: .grouped)
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: String(describing: UITableViewCell.self))
+        tableView.dataSource = self
+        tableView.delegate = self
+        return tableView
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Pickle"
+        tableView.frame = view.bounds
+        tableView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        view.addSubview(tableView)
+        view.backgroundColor = .white
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        if let indexPath = tableView.indexPathForSelectedRow {
+            tableView.deselectRow(at: indexPath, animated: true)
+        }
+    }
+
+    // MARK: - UITableViewDataSource
+
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return Section.count
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: String(describing: UITableViewCell.self), for: indexPath)
+        cell.textLabel?.text = Section(rawValue: indexPath.section)?.title
+        cell.accessoryType = .disclosureIndicator
+        return cell
+    }
+
+    // MARK: - UITableViewDelegate
+
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        switch Section(rawValue: indexPath.section) {
+        case .some(.default):
+            let picker = ImagePickerController()
+            picker.delegate = self
+            present(picker, animated: true, completion: nil)
+        default:
+            break
+        }
+    }
+
+    // MARK: - ImagePickerControllerDelegate
+
+    func imagePickerController(_ picker: ImagePickerController, shouldLaunchCameraWithAuthorization status: AVAuthorizationStatus) -> Bool {
+        return true
+    }
+
+    func imagePickerController(_ picker: ImagePickerController, didFinishPickingImageAssets assets: [PHAsset]) {
+        debugPrint(assets)
+        picker.dismiss(animated: true, completion: nil)
+    }
+
+    func imagePickerControllerDidCancel(_ picker: ImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
+    }
 
 }
