6ab1f1ad8bd24da1c99686e495471277c608c9cd
Ben <b.cylin@gmail.com>
2018-09-30 21:22:10 +0100

    Fix SwiftLint warnings

--- Pickle/Classes/ImagePickerController.swift
+++ Pickle/Classes/ImagePickerController.swift
@@ -226,7 +226,7 @@ extension ImagePickerController: UIImagePickerControllerDelegate, UINavigationCo
         }
 
         // Instead of using UIImagePickerControllerEditedImage, crop the original image for higher resolution if UIImagePickerControllerCropRect is specified.
-        var croppedImage: UIImage? = nil
+        var croppedImage: UIImage?
         if let cropRect = info[UIImagePickerControllerCropRect] as? CGRect, let cgImage = originalImage.cgImage?.cropping(to: cropRect) {
             croppedImage = UIImage(cgImage: cgImage, scale: originalImage.scale, orientation: originalImage.imageOrientation)
         }
@@ -296,7 +296,7 @@ extension ImagePickerController: PhotoGalleryViewControllerDelegate {
         } else {
             switch allowedSelections {
             case .limit(to: let number) where 1 < number && selectedAssets.count < number:
-                fallthrough // swiftlint:disable:this fallthrough
+                fallthrough // swiftlint:disable:this no_fallthrough_only
             case .unlimited:
                 selectedAssets.append(asset)
                 imagePickerDelegate?.imagePickerController?(self, didSelectImageAsset: asset)
