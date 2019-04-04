87f2273540e4127aadd1c9d46941f6c99e156428
huyduc <dave@thecarousell.com>
2019-04-04 13:42:41 +0800

    Add new camera button

---
+++ LiveView.swift
@@ -0,0 +1,8 @@
+//
+//  LiveView.swift
+//  Pickle
+//
+//  Created by Dave Luong on 4/4/19.
+//
+
+import Foundation
---
+++ Pickle/Classes/PhotoGalleryLiveViewCell.swift
@@ -0,0 +1,8 @@
+//
+//  PhotoGalleryLiveViewCell.swift
+//  Pickle
+//
+//  Created by Dave Luong on 4/4/19.
+//
+
+import Foundation
